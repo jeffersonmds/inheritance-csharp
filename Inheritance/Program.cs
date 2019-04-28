ef3cc3c63476e0f2aec2e0f9c18eea897ae66836
Jefferson Morais <jeffmorais96@gmail.com>
2019-04-28 00:52:06 -0300

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Inheritance.Entities;
+using System.Globalization;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char option = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (option == 'c') 
+                    products.Add(new Product(name, price));
+                else if (option == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, date));
+                }
+                else if (option == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+            Console.WriteLine("\n\nPRICE TAGS:");
+            foreach(Product p in products)
+            {
+                Console.WriteLine(p.PriceTag());
+            }
+        }
+    }
+}
